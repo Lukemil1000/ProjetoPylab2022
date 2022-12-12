98861c8f6ad17887c3fe660817a3621a127c1880
Lucas Pomar <lucaspomar@outlook.com>
2022-12-12 19:35:04 -0300

    Models Empresa e Tecnologias

--- seletive/empresa/models.py
+++ seletive/empresa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Tecnologias(models.Model):
+    tecnologia = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tecnologia
+
+
+class Empresa(models.Model):
+    choice_nicho_mercado = (
+        ('M', 'Marketing'),
+        ('N', 'Nutrição'),
+        ('D', 'Design')
+    )
+    nome = models.CharField(max_length=30)
+    email = models.EmailField()
+    cidade = models.CharField(max_length=30)
+    endereco = models.CharField(max_length=30)
+    caracteristica_empresa = models.TextField()
+    nicho_mercado = models.CharField(max_length=3, choices=choice_nicho_mercado)
+    logo = models.ImageField(upload_to="logo_empresa", null=True)
+    tecnologias = models.ManyToManyField(Tecnologias)
+
+    def __str__(self):
+        return self.nome
